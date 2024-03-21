04661ce6ca81b1452e80af45eae6cd9a7b4242b3
Susanmwende <mwendezmasaku@gmail.com>
2024-03-21 22:58:34 +0300

    form

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let slideIndex = 0;
-slideShows();
+// let slideIndex = 0;
+// showsSlide();
 
-function slideShows() {
-    let s;
-    let slide = document.getElementsByClassName("mySlide");
-    let dots = document.getElementsByClassName("dot")
-    for (s = 0;s<slide.length; s++){
-        slide[s].style.display ="none"
-}
+// function slideShows() {
+//     let i;
+//     let slide = document.getElementsByClassName("mySlide");
+//     let dots = document.getElementsByClassName("dot")
+//     for (i = 0;i<slide.length; i++){
+//         slide[i].style.display ="none"
+// }
 
-slideIndex++;
-if (slideIndex > slide.length) {slideIndex= 1}
-    for(s = 0; s<dots.length; s++) {
-        dots[s].className= dots[s].className.replace("active","" );
-    }
+// slideIndex++;
+// if (slideIndex > slide.length) {slideIndex= 1}
+//     for(i = 0; s<dots.length; i++) {
+//         dots[i].className= dots[i].className.replace("active","" );
+//     }
 
 
-slides[slideIndex -1].stle.display ="block";
-dots[slideIndex-1].className+= "active";
-setTimeout(slideShows,2000);
-}
+// slides[slideIndex -1].stle.display ="block";
+// dots[slideIndex-1].className+= "active";
+// setTimeout(slideShows,2000);
+// }
