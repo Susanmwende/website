ab565f5a4a55f062d6a6b70e5bc419715ce5796d
Susanmwende <mwendezmasaku@gmail.com>
2024-03-27 12:06:50 +0300

    website edit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const slides = document.querySelectorAll(".slides img")
-let slideIndex = 0;
-let intervalId = null;
+const slides = document.querySelectorAll(".slides img") //variable  stores data of the of the images 
+let slideIndex = 0;// start index point
+let intervalId = null; // the slides were not in motion
 
-document.addEventListener("DOMContentLoaded",initializeSlider);
+document.addEventListener("DOMContentLoaded",initializeSlider); //responds to the click
 
     
 
@@ -10,13 +10,13 @@ document.addEventListener("DOMContentLoaded",initializeSlider);
 
     if(slides.length > 0){
         slides[slideIndex].classList.add("displaySlide");
-    intervalId = setInterval(nextSlide,5000);
+    intervalId = setInterval(nextSlide,5000); // 5000 is time in miliseconds(5 seconds)
     
   } 
 }
-  function showSlide(index){
+  function showSlide(index){ // the if statement is for if the images get to the last slide itgoes back to the first slide
 
-    if(index >= slides.length){
+    if(index >= slides.length){ 
         slideIndex = 0;
     }
 
@@ -26,19 +26,19 @@ document.addEventListener("DOMContentLoaded",initializeSlider);
 
 
     slides.forEach(slide => {
-        slide.classList.remove("displaySlide");
+        slide.classList.remove("displaySlide"); // compiles all the images to the slider
     });
 
-    slides[slideIndex].classList.add("displaySlide");
+    slides[slideIndex].classList.add("displaySlide"); // enables the images to slide to the next
 
   }
   function prevSlide(){
-    clearInterval(intervalId);
-    slideIndex--;
-    showSlide(slideIndex)
+    clearInterval(intervalId);// set each call time to fixed time delay
+    slideIndex--; // enables onee to tap on the prev button which should take them to the previous photo
+    showSlide(slideIndex) // links the previous button to the actual slider so that one is able to slide successfully
   }
 function nextSlide(){
-slideIndex++
-showSlide(slideIndex)
+slideIndex++ // enables one to tap on the next button which should  take one to the next image
+showSlide(slideIndex) // links the next button to next slider so that it is
 
 }
